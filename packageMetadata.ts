858a175ca452e47762a3fe69142c18fc1acd2ff4
Wojciech Odachowski <wojciech.odachowski@hotmail.com>
2022-05-12 21:53:01 +0200

    separate package metadata

---
+++ packageMetadata.ts
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         WnL customization
+// @namespace    http://tampermonkey.net/
+// @version      1.9.10
+// @description  NIEOFICJALNY asystent WnL
+// @author       wodac
+// @updateURL    https://github.com/wodac/wnl-customization/raw/dist/wnl-customization.user.js
+// @match        https://lek.wiecejnizlek.pl/app/*
+// @connect      https://lek.wiecejnizlek.pl/*
+// @icon         https://www.google.com/s2/favicons?domain=wiecejnizlek.pl
+// @grant        GM_registerMenuCommand
+// @grant        GM_unregisterMenuCommand
+// @grant        unsafeWindow
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_xmlhttpRequest
+// @run-at document-body
+// ==/UserScript==
