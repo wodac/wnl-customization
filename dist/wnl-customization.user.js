c355900c9b8c256a77289386d79565a340f4241b
wodac <68289847+wodac@users.noreply.github.com>
2022-05-13 11:53:05 +0000

    Deploying to gh-pages from @ wodac/wnl-customization@af6050ac2b809bad8d8d7bb64660be1790db6914 🚀

--- dist/wnl-customization.user.js
+++ dist/wnl-customization.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         WnL customization
 // @namespace    http://tampermonkey.net/
-// @version      1.9.14
+// @version      1.9.15
 // @description  NIEOFICJALNY asystent WnL
 // @author       wodac
-// @updateURL    https://github.com/wodac/wnl-customization/raw/dist/wnl-customization.user.js
+// @updateURL    https://wodac.github.io/wnl-customization/dist/wnl-customization.user.js
 // @match        https://lek.wiecejnizlek.pl/app/*
 // @connect      https://lek.wiecejnizlek.pl/*
 // @icon         https://www.google.com/s2/favicons?domain=wiecejnizlek.pl
@@ -16,8 +16,6 @@
 // @grant        GM_xmlhttpRequest
 // @run-at document-body
 // ==/UserScript==
-// test github actions
-// test deployment
 document = unsafeWindow.document;
 let toRunOnLoaded = [];
 let sidebarSettingsContainer = null;
