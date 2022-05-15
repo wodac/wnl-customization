5ffb82fa7ae200198af2d4389f6ad2e8f84445c1
Wojciech Odachowski <wojciech.odachowski@hotmail.com>
2022-05-15 14:57:06 +0200

    adjust metadata to beta branch

--- packageMetadata.ts
+++ packageMetadata.ts
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name         WnL customization
+// @name         WnL customization (beta)
 // @namespace    http://tampermonkey.net/
-// @version      1.9.27
+// @version      1.9.27b
 // @description  NIEOFICJALNY asystent WnL
 // @author       wodac
-// @updateURL    https://wodac.github.io/wnl-customization/dist/wnl-customization.user.js
+// @updateURL    https://wodac.github.io/wnl-customization/beta/wnl-customization.user.js
 // @match        https://lek.wiecejnizlek.pl/app/*
 // @connect      https://lek.wiecejnizlek.pl/*
 // @icon         https://www.google.com/s2/favicons?domain=wiecejnizlek.pl
