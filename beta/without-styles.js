1a665dba571cbdc5b8fb61b081f3895b6f95ccb6
wodac <68289847+wodac@users.noreply.github.com>
2022-06-19 20:51:56 +0000

    Deploying to gh-pages from @ wodac/wnl-customization@6aa21741c399d1f32958c9e71e55b03f8bbb4e10 🚀

--- beta/without-styles.js
+++ beta/without-styles.js
@@ -2141,6 +2141,10 @@ const getOptions = (app) => [
             }
         },
         onrender: function () {
+            if (isMobile()) {
+                this.disabled = true;
+                return;
+            }
             mouseTimer.addEventListener('timerEnd', () => {
                 setMouseVisible(false);
                 document.addEventListener('mousemove', () => setMouseVisible(true));
